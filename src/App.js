0cf4466f91bd7bfd304f86744639508c67839d46
evaw277 <evaweisneck@gmail.com>
2023-06-09 20:45:01 -0400

    Added add task form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React from "react";
-
-const todoList = [
-  {
-    id: 1,
-    title: "code",
-  },
-  {
-    id: 2,
-    title: "go to store",
-  },
-  {
-    id: 3,
-    title: "attend group session",
-  },
-];
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map(function (item) {
-          return <li key={item.id}>{item.title}</li>;
-        })}
-      </ul>
+      <AddTodoForm />
+      <TodoList />
     </div>
   );
 }
