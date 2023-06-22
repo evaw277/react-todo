4d166d6a436fe461c56ae7aa7757a9bd24881247
evaw277 <evaweisneck@gmail.com>
2023-06-21 22:19:16 -0400

    added callback function to App

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function AddTodoForm() {
+export default function AddTodoForm(props) {
   function handleAddTodo(event) {
     event.preventDefault();
     const todoTitle = event.target.title.value;
     console.log(todoTitle);
+    props.onAdd(event);
     event.target.reset();
   }
   return (
--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
 function App() {
+  const [newTodo, setNewTodo] = React.useState(" ");
+
+  function onAddTodo(event) {
+    setNewTodo(event.target.title.value);
+  }
+
   return (
     <div>
       <h1>Todo List</h1>
-      <AddTodoForm />
+      <AddTodoForm onAdd={onAddTodo} />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
