2570cec5980b2a334c56f3ace1bce08aebadcc03
evaw277 <evaweisneck@gmail.com>
2023-07-25 14:36:12 -0400

    fixed git merge error

--- src/App.js
+++ src/App.js
@@ -21,6 +21,12 @@ function App() {
     JSON.parse(localStorage.getItem("savedTodoList")) || []
   );
 
+  // useEffect(() => {
+  //   console.log("check")
+  //   new Promise((resolve, reject) => {
+  //   setTimeout(() => { resolve({data: {todoList: []}})}, 2000 ).then(result)
+  //   })
+
   useEffect(() => {
     localStorage.setItem("savedTodoList", JSON.stringify(todoList));
   }, [todoList]);
@@ -28,12 +34,17 @@ function App() {
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
   }
+  const removeTodo = (id) => {
+    const removeItem = todoList.filter((todo) => todo.id !== id);
+
+    setTodoList(removeItem);
+  };
 
   return (
     <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} removeTodo={removeTodo} />
     </>
   );
 }
