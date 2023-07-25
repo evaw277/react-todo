e5ee5b4cfeeb8530423c3f9a674b808d8a352124
evaw277 <evaweisneck@gmail.com>
2023-07-25 17:10:22 -0400

    Async part 1

--- src/App.js
+++ src/App.js
@@ -17,16 +17,26 @@ import AddTodoForm from "./AddTodoForm";
 function App() {
   // const [todoList, setTodoList] = useSemiPersistentState("savedTodoList");
 
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem("savedTodoList")) || []
-  );
+  const [todoList, setTodoList] = useState([]);
 
-  // useEffect(() => {
-  //   console.log("check")
-  //   new Promise((resolve, reject) => {
-  //   setTimeout(() => { resolve({data: {todoList: []}})}, 2000 ).then(result)
-  //   })
+  useEffect(() => {
+    console.log("check run");
+    new Promise((resolve, reject) => {
+      setTimeout(
+        () =>
+          resolve({
+            data: {
+              todoList: JSON.parse(localStorage.getItem("savedTodoList")),
+            },
+          }),
+        2000
+      );
+    });
+  });
 
+  // .then(result) =>
+  //          setTodoList(result.data.todoList)
+  //
   useEffect(() => {
     localStorage.setItem("savedTodoList", JSON.stringify(todoList));
   }, [todoList]);
