dfdbc3656c2749659d26bb24f087cc71f925c73c
evaw277 <evaweisneck@gmail.com>
2023-07-27 20:53:45 -0400

    removed dev notes/console logs

--- src/App.js
+++ src/App.js
@@ -2,26 +2,11 @@ import React, { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
-// const useSemiPersistentState = () => {
-//   const [todoList, setTodoList] = useState(
-//     JSON.parse(localStorage.getItem("savedTodoList")) || []
-//   );
-
-//   useEffect(() => {
-//     localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-//   }, [todoList]);
-
-//   return [todoList, setTodoList];
-// };
-
 function App() {
-  // const [todoList, setTodoList] = useSemiPersistentState("savedTodoList");
-
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    console.log("check");
     new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve({
@@ -31,7 +16,6 @@ function App() {
         });
       }, 2000);
     }).then((result) => {
-      console.log(result);
       setTodoList(result.data.todoList);
       setIsLoading(false);
     });
