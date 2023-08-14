620bb3ae3fdc469ff554d3cb134ff2a5291f2b61
evaw277 <evaweisneck@gmail.com>
2023-08-14 13:35:13 -0400

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -59,15 +60,24 @@ function App() {
   };
 
   return (
-    <>
-      <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo} />
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <TodoList todoList={todoList} removeTodo={removeTodo} />
-      )}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Todo List</h1>
+              <AddTodoForm onAddTodo={addTodo} />
+              {isLoading ? (
+                <p>Loading...</p>
+              ) : (
+                <TodoList todoList={todoList} removeTodo={removeTodo} />
+              )}
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
