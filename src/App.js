406afb3a26857dc30ddcfbdfc29b7b8669511767
evaw277 <evaweisneck@gmail.com>
2023-09-26 21:45:42 -0400

    added Landing Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TodoContainer from "./components/TodoContainer";
+import LandingPage from "./components/LandingPage";
 // import { FontAwesomeIcon } from "@fortawesome/react-font-awesome";
 
 // const icon = <FontAwesomeIcon icon={faSquareCheck} />;
@@ -8,8 +9,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TodoContainer />} />
-        <Route path="/new" element={<h1>New Todo List</h1>} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/todolist" element={<TodoContainer />}></Route>
       </Routes>
     </BrowserRouter>
   );
